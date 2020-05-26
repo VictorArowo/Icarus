eeba05baf43ae09c5678d96efa83d905898a43a3
Victor Arowo <arowovici@gmail.com>
2020-05-26 20:45:29 +0100

    More Work on Dashboard

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -8,6 +8,7 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthenticationContext";
 import Loading from "../components/Loading";
 import PlusIcon from "../icons/PlusIcon";
+import BookIcon from "../icons/BookIcon";
 
 const Dashboard: NextPage = ({ token }: any) => {
   useAuth({ token });
@@ -39,17 +40,50 @@ const Dashboard: NextPage = ({ token }: any) => {
         </div>
       </div>
       <div className="mt-10">
-        <div className="w-full py-4 shadow px-7 md:w-1/2">
+        <div className="w-full px-5 py-10 shadow px-7 md:w-3/4">
           <div className="flex items-baseline text-white">
             <p className="text-4xl font-bold font-white">3,000</p>
             <p className="ml-2 text-lg">responses this week</p>
           </div>
-          <div className="mt-2">
-            <div className="w-1/3"></div>
-            <div className="w-1/3"></div>
-            <div className="w-1/3"></div>
+          <div className="flex mt-5">
+            <div className="flex w-1/3 h-16">
+              <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 bg-purple-200 rounded-full">
+                <div className="w-6 h-6 text-purple-800">
+                  <BookIcon />
+                </div>
+              </div>
+              <div className="ml-2 text-white">
+                <p className="text-xl">15</p>
+                <p className="text-gray-400">total forms</p>
+              </div>
+            </div>
+            <div className="flex w-1/3 h-16">
+              <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 bg-yellow-100 rounded-full">
+                <div className="w-6 h-6 text-yellow-800 ">
+                  <BookIcon />
+                </div>
+              </div>
+              <div className="ml-2 text-white">
+                <p className="text-xl">5000</p>
+                <p className="text-gray-400">total responses</p>
+              </div>
+            </div>
+            <div className="flex w-1/3 h-16">
+              <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 bg-green-200 rounded-full">
+                <div className="w-6 h-6 text-green-800">
+                  <BookIcon />
+                </div>
+              </div>
+              <div className="ml-2 text-white">
+                <p className="text-xl">4</p>
+                <p className="text-gray-400">active forms</p>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="mt-10">
+        <h1 className="text-2xl font-bold text-white">Recent Activity</h1>
       </div>
       <div>{!isAuthenticated && <Loading />}</div>
     </Layout>
