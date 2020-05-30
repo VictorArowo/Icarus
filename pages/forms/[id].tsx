fc7310295ba2cf39fe5bec19ad8081058eac1d3a
Victor Arowo <arowovici@gmail.com>
2020-05-30 13:41:21 +0100

    Keys to mapping

--- pages/forms/[id].tsx
+++ pages/forms/[id].tsx
@@ -15,23 +15,32 @@ const FormDetails = () => {
       return acc;
     }, {});
   }, [selected]);
-  console.log("responses", responses);
+
   return (
     <Layout>
       <div className="flex flex-col">
         <h1 className="text-3xl text-center text-gray-100">{selected.title}</h1>
         <h3 className="text-center text-gray-400">{selected.description}</h3>
         <div className="mt-10">
-          {selected.body.map((question) => {
-            return (
-              <div>
-                <p className="text-2xl text-gray-100">{question.title}</p>
-                {responses[question.id].map((response) => (
-                  <div>{response}</div>
+          {selected.body.map((question, index) => (
+            <div className="px-5 py-10 mb-5 rounded-lg shadow-xl" key={index}>
+              <p className="text-2xl font-bold text-gray-100 ">
+                <span className="text-lg text-primary">Q{index + 1}</span>
+                {"    "}
+                {question.title}
+              </p>
+              <p className="text-sm text-gray-300">
+                {responses[question.id].length} reponses
+              </p>
+              <div className="mt-2">
+                {responses[question.id].map((response, index) => (
+                  <div className="text-gray-100" key={index}>
+                    {response}
+                  </div>
                 ))}
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </Layout>
